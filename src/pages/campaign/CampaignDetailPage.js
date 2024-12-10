c56af3e2bc3d74684c067050411ea242843ba517
FangHaiWu <vuhuyphuong8x@gmail.com>
2024-12-10 20:33:29 +0700

    Campaign Rating

--- src/pages/campaign/CampaignDetailPage.js
+++ src/pages/campaign/CampaignDetailPage.js
@@ -4,45 +4,49 @@ import axiosInstance from "../../utils/axiosInstance";
 import { useParams } from "react-router-dom";
 import CampaignDetail from "../../components/campaign/CampaignDetail";
 import CommentSection from "../../components/comment/CommentSection";
-import DonationWidget from "../../components/widget/DonationWidget";
+import DonationWidget from "../../components/widget/DonateionWidget";
+import RatingSection from "../../components/rating/RatingSection";
 
 const CampaignDetailPage = () => {
-	const [campaign, setCampaign] = useState(null);
+    const [campaign, setCampaign] = useState(null);
 
-	const { id } = useParams();
+    const { id } = useParams();
 
-	const fetchCampaignDetail = useCallback(async () => {
-		try {
-			const response = await axiosInstance.get(`/campaign/${id}`);
-			setCampaign(response.data);
-		} catch (error) {
-			console.error("Error fetching campaign details:", error);
-		}
-	}, [id]);
+    const fetchCampaignDetail = useCallback(async () => {
+        try {
+            const response = await axiosInstance.get(`/campaign/${id}`);
+            setCampaign(response.data);
+        } catch (error) {
+            console.error("Error fetching campaign details:", error);
+        }
+    }, [id]);
 
-	useEffect(() => {
-		fetchCampaignDetail();
-	}, [fetchCampaignDetail]);
+    useEffect(() => {
+        fetchCampaignDetail();
+    }, [fetchCampaignDetail]);
 
-	if (!campaign) {
-		return <Typography>Loading campaign details...</Typography>;
-	}
+    if (!campaign) {
+        return <Typography>Loading campaign details...</Typography>;
+    }
 
-	return (
-		<Container
-			maxWidth={false}
-			className='mt-6'
-			sx={{ display: "flex", justifyContent: "space-between" }}
-		>
-			<Box sx={{ width: "70%" }}>
-				<CampaignDetail campaign={campaign} />
-				<CommentSection campaignId={id} />
-			</Box>
-			<Box sx={{ width: "28%" }}>
-				<DonationWidget campaignId={id} unitOfMeasurement={campaign.unitOfMeasurement} />
-			</Box>
-		</Container>
-	);
+    return (
+        <Container
+            maxWidth={false}
+            className="mt-6"
+            sx={{ display: "flex", justifyContent: "space-between" }}
+        >
+            <Box sx={{ width: "70%" }}>
+                <CampaignDetail campaign={campaign} />
+                <RatingSection campaignId={id} />
+                <CommentSection campaignId={id} />
+            </Box>
+            <Box sx={{ width: "28%" }}>
+                <DonationWidget
+                    unitOfMeasurement={campaign.unitOfMeasurement}
+                />
+            </Box>
+        </Container>
+    );
 };
 
 export default CampaignDetailPage;
