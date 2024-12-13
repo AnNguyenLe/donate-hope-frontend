253b69c5369946926fcfeeb0c488f9ed3c3fb2f2
FangHaiWu <vuhuyphuong8x@gmail.com>
2024-12-13 14:28:37 +0700

    Commit my changes before pulling

--- src/pages/campaign/CampaignDetailPage.js
+++ src/pages/campaign/CampaignDetailPage.js
@@ -8,46 +8,51 @@ import DonationWidget from "../../components/widget/DonationWidget";
 import RatingSection from "../../components/rating/RatingSection";
 
 const CampaignDetailPage = () => {
-  const [campaign, setCampaign] = useState(null);
+    const [campaign, setCampaign] = useState(null);
 
-  const { id } = useParams();
+    const { id } = useParams();
 
-  const fetchCampaignDetail = useCallback(async () => {
-    try {
-      const response = await axiosInstance.get(`/campaign/${id}`);
-      setCampaign(response.data);
-    } catch (error) {
-      console.error("Error fetching campaign details:", error);
+    const fetchCampaignDetail = useCallback(async () => {
+        try {
+            const response = await axiosInstance.get(`/campaign/${id}`);
+            setCampaign(response.data);
+        } catch (error) {
+            console.error("Error fetching campaign details:", error);
+        }
+    }, [id]);
+
+    useEffect(() => {
+        fetchCampaignDetail();
+    }, [fetchCampaignDetail]);
+
+    if (!campaign) {
+        return <Typography>Loading campaign details...</Typography>;
     }
-  }, [id]);
 
-  useEffect(() => {
-    fetchCampaignDetail();
-  }, [fetchCampaignDetail]);
-
-  if (!campaign) {
-    return <Typography>Loading campaign details...</Typography>;
-  }
-
-  return (
-    <Container
-      maxWidth={false}
-      className="mt-6"
-      sx={{ display: "flex", justifyContent: "space-between" }}
-    >
-      <Box sx={{ width: "70%" }}>
-        <CampaignDetail campaign={campaign} />
-        <RatingSection campaignId={id} />
-        <CommentSection campaignId={id} />
-      </Box>
-      <Box sx={{ width: "28%" }}>
-        <DonationWidget
-          campaignId={id}
-          unitOfMeasurement={campaign.unitOfMeasurement}
-        />
-      </Box>
-    </Container>
-  );
+    return (
+        <Container
+            maxWidth={false}
+            className="pt-6"
+            sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                // background: "url(/bg-detail-green.jpg)",
+                backgroundSize: "cover",
+            }}
+        >
+            <Box sx={{ width: "70%" }}>
+                <CampaignDetail campaign={campaign} />
+                <RatingSection campaignId={id} />
+                <CommentSection campaignId={id} />
+            </Box>
+            <Box sx={{ width: "28%" }}>
+                <DonationWidget
+                    campaignId={id}
+                    unitOfMeasurement={campaign.unitOfMeasurement}
+                />
+            </Box>
+        </Container>
+    );
 };
 
 export default CampaignDetailPage;
